80513626cc183eab0bb1a554e96aac0338f867af
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-11-12 19:59:37 -0300

    Initial commit: estrutura do repositório e primeiras aulas de Python

---
+++ aula1.py
@@ -0,0 +1,25 @@
+#Operadores aritméticos
+
+#soma 
+
+soma = 2 + 2
+print(soma)
+
+subtracao = 2 - 1
+print(subtracao)
+
+multiplicacao = 2 * 8
+print(multiplicacao)
+
+divisao = 6 / 3
+print(divisao)
+
+exponenciacao = 2 ** 3
+print(exponenciacao)
+
+restoDaDiv = 3 % 4
+print(restoDaDiv)
+
+divChao = 3 // 4
+print(divChao)
+
