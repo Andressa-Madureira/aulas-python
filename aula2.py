96bdd5c42756b01895702389cfb5862b995dfec3
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-11-12 20:12:42 -0300

    Adiciona aula 2: estruturas de dados em Python

---
+++ aula2.py
@@ -0,0 +1,21 @@
+#Variáveis
+
+nome = "Andressa"
+print(f'Meu nome é',nome )
+
+#variáveis numéricas 
+
+numero_inteiro_1 = 100
+numero_inteiro_2 = int(200)
+print(numero_inteiro_1,numero_inteiro_2)
+
+#variáveis flutuantes
+numero_flutuante1 = 10.90
+numero_flutuante2 = float(10.50)
+print(numero_flutuante1,numero_flutuante2)
+
+#variáveis boolean
+
+var_boolean_01 = True
+var_boolean_02 = False
+print(var_boolean_01,var_boolean_02)
